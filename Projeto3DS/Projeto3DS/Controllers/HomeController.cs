b1d5dce30e1b2a000b5c05d8fb49557044696862
MatheusGamaRussi <matheus.russi@etec.sp.gov.br>
2024-02-29 21:24:59 -0300

    Commit inicial

---
+++ Projeto3DS/Projeto3DS/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Projeto3DS.Models;
+using System.Diagnostics;
+
+namespace Projeto3DS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
